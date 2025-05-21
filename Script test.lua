c4b93a8da4dcd1b1e5cc59ab3c2dc5707dcde829
Zeca130 <150437474+Zeca130@users.noreply.github.com>
2025-05-21 21:52:22 +0200

    Update Script test.lua

--- Script test.lua
+++ Script test.lua
@@ -1,16 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/Doors-mode/refs/heads/main/README.md?token=GHSAT0AAAAAADEHFJJ2IRGZMD2BHXYTOPII2BOFIQA"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/Doors-mode/refs/heads/main/Text.lua?token=GHSAT0AAAAAADEHFJJ3QELDNJS7UUGKBRRS2BOFKVQ"))()
-
-wait(180)
-
-while true do
-    loadstring(game:HttpGet("https://pastebin.com/raw/yMVAwKk2"))()
-    wait(180)
-end
-wait(55)
-
-while true do
-    loadstring(game:HttpGet('https://pastefy.app/BItVDA0u/raw'))()
-    wait(55)
-end
