ed7438c0b711c9cc167e1eabab80003b6bae6eef
Zeca130 <150437474+Zeca130@users.noreply.github.com>
2025-05-22 10:05:40 +0200

    Update Script test.lua

--- Script test.lua
+++ Script test.lua
@@ -1,1 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DoorsHubs/Doors-Hubs/refs/heads/main/Hardcore%20Sprint%20System%20(PC%20and%20Mobile%20Adaptation).lua.txt"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/Doors-mode/refs/heads/main/Text.lua?token=GHSAT0AAAAAADEHFJJ3R4DEXDRUUWMMNKW22BO3AQA"))()
