e24871cdf6f2f631cf20b2623235af3721533831
Zeca130 <150437474+Zeca130@users.noreply.github.com>
2025-05-22 16:31:04 +0200

    Update Script test.lua

--- Script test.lua
+++ Script test.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/Doors-mode/refs/heads/main/README.md"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/Doors-mode/refs/heads/main/Text.lua"))()
