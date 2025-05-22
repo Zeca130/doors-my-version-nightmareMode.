7b8d7fb956ab1bb0f794b3dbaac7058955b09bd0
Zeca130 <150437474+Zeca130@users.noreply.github.com>
2025-05-22 18:04:34 +0200

    Update Doors mini nightmare mode0.7byzc140x.lua

--- Doors mini nightmare mode0.7byzcp140.lua
+++ Doors mini nightmare mode0.7byzcp140.lua
@@ -1,43 +1,43 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/Doors-mode/refs/heads/main/README.md"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/Doors-mode/refs/heads/main/Text.lua"))()
 task.spawn(function()
-    wait(180)
+    wait(230)
     
     while true do
         pcall(function()
             loadstring(game:HttpGet("https://pastebin.com/raw/yMVAwKk2"))()
         end)
-        wait(180)
+        wait(230)
     end
 end)
 task.spawn(function()
-    wait(50)
+    wait(90)
     
     while true do
         pcall(function()
             loadstring(game:HttpGet("https://pastefy.app/BItVDA0u/raw"))()
         end)
-        wait(50)
+        wait(90)
     end
 end)
 task.spawn(function()
-    wait(95)
+    wait(120)
     
     while true do
         pcall(function()
             loadstring(game:HttpGet('https://raw.githubusercontent.com/Kotyara19k-Doorsspawner/Random-files/main/TraumaSoucreSpawner'))()
         end)
-        wait(95)
+        wait(120)
     end
 end)
 task.spawn(function()
-    wait(130)
+    wait(195)
     
     while true do
         pcall(function()
             loadstring(game:HttpGet('https://raw.githubusercontent.com/Zeca130/Doors-mode/refs/heads/main/Entity5'))()
         end)
-        wait(130)
+        wait(195)
     end
 end)
 task.spawn(function()
