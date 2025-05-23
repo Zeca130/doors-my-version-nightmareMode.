228f874dff4586d46a81618f12a92ea037a04ea9
Zeca130 <150437474+Zeca130@users.noreply.github.com>
2025-05-23 16:55:49 +0200

    Doors nightmare mode 1.2Beta.lua

---
+++ Doors mini nightmare mode 1.2Beta.lua
@@ -0,0 +1,88 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/Doors-mode/refs/heads/main/README.md"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/doors-my-version-nightmareMode./refs/heads/main/Text%20for%20test%20update%20doors%20script"))()
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Zeca130/doors-my-version-nightmareMode./refs/heads/main/Fog'))()
+    end)
+end)
+task.spawn(function()
+    wait(230)
+    
+    while true do
+        pcall(function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/yMVAwKk2"))()
+        end)
+        wait(320)
+    end
+end)
+task.spawn(function()
+    wait(90)
+    
+    while true do
+        pcall(function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/doors-my-version-nightmareMode./refs/heads/main/Entity3.txt"))()
+        end)
+        wait(130)
+    end
+end)
+task.spawn(function()
+    wait(220)
+    
+    while true do
+        pcall(function()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/Zeca130/doors-my-version-nightmareMode./refs/heads/main/Entity4.txt'))()
+        end)
+        wait(280)
+    end
+end)
+task.spawn(function()
+    wait(210)
+    
+    while true do
+        pcall(function()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/Zeca130/Doors-mode/refs/heads/main/Entity5'))()
+        end)
+        wait(250)
+    end
+end)
+task.spawn(function()
+    wait(40)
+    
+    while true do
+        pcall(function()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/Zeca130/doors-my-version-nightmareMode./refs/heads/main/New%20entity'))()
+        end)
+        wait(35)
+    end
+end)
+task.spawn(function()
+    wait(290)
+    
+    while true do
+        pcall(function()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/Zeca130/doors-my-version-nightmareMode./refs/heads/main/Entity6'))()
+        end)
+        wait(380)
+    end
+end)
+task.spawn(function()
+    wait(450)
+    
+    while true do
+        pcall(function()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/Idk-lol2/a-60aa/refs/heads/main/---%3D%3D%3D%3D%3D%3D%20a-60%20agresiv%20spawner%20%3D%3D%3D%3D%3D%3D---.txt'))()
+        end)
+        wait(600)
+    end
+end)
+task.spawn(function()
+    wait(350)
+    
+    while true do
+        pcall(function()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/Zeca130/doors-my-version-nightmareMode./refs/heads/main/S.R%20Entity'))()
+        end)
+        wait(480)
+    end
+end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/doors-my-version-nightmareMode./refs/heads/main/Floor%202"))()
