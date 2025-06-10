ccd51f3a2a9cd0dc80cc8d6ae9e5d5e914acc528
Zeca130 <150437474+Zeca130@users.noreply.github.com>
2025-06-10 16:38:56 +0200

    Update Doors nightmare mode V3.5.lua

--- Doors nightmare mode V3.5.lua
+++ Doors nightmare mode V3.5.lua
@@ -102,7 +102,7 @@ wait(900)
     
     while true do
         pcall(function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/doors-my-version-nightmareMode./refs/heads/main/Red%20face"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/doors-my-version-nightmareMode./refs/heads/main/Red%20face.lua"))()
         end)
         wait(990)
     end
