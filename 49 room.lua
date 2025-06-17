b63b155c11c8a3fd806bc30e5dfc06b63077c609
Zeca130 <150437474+Zeca130@users.noreply.github.com>
2025-06-17 19:01:47 +0200

    Update and rename 49 room to 49 room.lua

--- 49 room.lua
+++ 49 room.lua
@@ -1,3 +1,3 @@
 local targetRoom = 48
 repeat game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait() until game.ReplicatedStorage.GameData.LatestRoom.Value == targetRoom
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/doors-my-version-nightmareMode./refs/heads/main/49%20room"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/doors-my-version-nightmareMode./refs/heads/main/LIBRARY.lua"))()
