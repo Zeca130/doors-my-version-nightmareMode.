cafcd8cbc89f515d485265547964f96412d5ba47
Zeca130 <150437474+Zeca130@users.noreply.github.com>
2025-07-12 09:16:44 +0200

    Update SPAWN ENTITY SILENT.lua

--- SPAWN ENTITY SILENT.lua
+++ SPAWN ENTITY SILENT.lua
@@ -1,10 +1,10 @@
 task.spawn(function()
-wait(100)
+wait(300)
     
     while true do
         pcall(function()
             loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeca130/doors-my-version-nightmareMode./refs/heads/main/Silent.lua"))()
         end)
-        wait(150)
+        wait(380)
     end
 end)
